d8319d7bf04b0f2a379fa74cb12bdcc030763aab
abdellani <abdellani@gmail.com>
2019-08-26 22:55:47 +0100

    Prepare for devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190826205102) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.string "token_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
