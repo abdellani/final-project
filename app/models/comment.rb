63cd86976a338ca97d308b6e68ca7e7bd0c47d34
abdellani <abdellani@gmail.com>
2019-08-29 20:39:27 +0100

    Add test files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  validates :content, length: {maximum: 255,minimum: 10},presence: true
+  validates :content, length: {maximum: 255},presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Content" do
+    before :each do 
+      @creator=User.create(name:"creator",email:"creator@email.com",password:"123465")
+      @creator.posts.create(con)
+    end
+    it "should be not be empty" do
+       
+    end
+    it "should not exceed 255 characters" do 
+    end
+    it "should belong to an author" do 
+    end
+    it "should belong to a post" do 
+    end
+  end
 end
