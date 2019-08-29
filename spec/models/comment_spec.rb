693f6489462014becd1a4048c74dcebd4b130ad8
abdellani <abdellani@gmail.com>
2019-08-29 21:13:01 +0100

    Add comment model tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,16 +4,25 @@ RSpec.describe Comment, type: :model do
   describe "Content" do
     before :each do 
       @creator=User.create(name:"creator",email:"creator@email.com",password:"123465")
-      @creator.posts.create(con)
+      @post=@creator.posts.create(content:"New post")
+      @comment=@post.comments.create(content:"this a comment",user_id:@creator.id)
+      expect(@comment.valid?).to eql(true)
     end
     it "should be not be empty" do
-       
+      @comment.content=""
+      expect(@comment.valid?).to eql(false)
     end
     it "should not exceed 255 characters" do 
-    end
+      @comment.content= "A"*256
+      expect(@comment.valid?).to eql(false)
+   end
     it "should belong to an author" do 
+      @comment.user= nil
+      expect(@comment.valid?).to eql(false)
     end
     it "should belong to a post" do 
+      @comment.post = nil
+      expect(@comment.valid?).to eql(false)
     end
   end
 end
