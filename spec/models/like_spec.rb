a5fe1d4d6365cad7e689164f2035ccb8139791aa
EmirVatric <vatricemir@gmail.com>
2019-08-29 23:10:25 +0200

    likes model and integration tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'test', email: 'test@test.com', password: '123456')
+    @post = @user.posts.create(content: 'test post')
+  end
+  
+  it 'should have post_id' do
+    @like = @user.likes.build()
+    @like.valid?
+    expect(@like.errors[:post]).to include("must exist")
+
+    @like = @user.likes.build(post_id: @post.id)
+    expect(@like.valid?).to eql(true)
+  end
+
+  it 'should have user_id' do
+    @like = @post.likes.build()
+    @like.valid?
+    expect(@like.errors[:user]).to include("must exist")
+
+    @like = @post.likes.build(user_id: @user.id)
+    expect(@like.valid?).to eql(true)
+  end
+
+  it 'cant be on same post twice from same user' do
+    @like = @post.likes.create(user_id: @user.id)
+
+    @like2 = @post.likes.build(user_id: @user.id)
+    expect(@like2.valid?).to eql(false)
+    expect(@like2.errors[:user_id]).to include("has already been taken")
+  end
 end
