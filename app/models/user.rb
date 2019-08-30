069a3afb20022dc935e688a8c13dac02fa2f3499
abdellani <abdellani@gmail.com>
2019-08-30 16:07:53 +0100

    Add friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,23 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  has_many :friendship_sent, class_name:"Friendship", 
+            foreign_key:"user1_id", dependent: :destroy
+  has_many :friendship_received, class_name:"Friendship",
+            foreign_key:"user2_id",dependent: :destroy
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email,format: { with: VALID_EMAIL_REGEX }
   validates :name, presence: true
+
+  def find_friends
+    User.find_by_sql(["SELECT * FROM users WHERE id IN (SELECT CASE WHEN user1_id=? THEN user2_id ELSE user1_id END FROM friendships WHERE (user1_id= ? OR user2_id = ?) AND status = true )",self.id,self.id,self.id]) 
+    
+  end
+  def pending_friends
+    User.find_by_sql(["SELECT * FROM users WHERE id IN (SELECT CASE WHEN user1_id=? THEN user2_id ELSE user1_id END FROM friendships WHERE (user1_id= ? OR user2_id = ?) AND status = false )",self.id,self.id,self.id]) 
+  end
+
   private
     def add_profile_image
       gravatar_id = Digest::MD5::hexdigest(self.email.downcase)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190829172700) do
+ActiveRecord::Schema.define(version: 20190829220434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20190829172700) do
     t.string "content"
     t.integer "user_id"
     t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user1_id"
+    t.integer "user2_id"
+    t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
