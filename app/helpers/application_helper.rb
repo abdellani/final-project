546d554bcc2d8811d31520cba8228f56ce6e9e88
abdellani <abdellani@gmail.com>
2019-08-30 23:31:49 +0100

    Fix linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,12 @@
 
 module ApplicationHelper
   def accept_reject_pending(user_id)
-    if current_user.find_friendships_pending(user_id).first && current_user.find_friendships_pending(user_id).first.receiver == current_user
-      content_tag(:li, link_to('Accept friendship', friendship_path(id: user_id), method: :put)) +
-        content_tag(:li, link_to('Reject friendship', friendship_path(id: user_id, status: 'pending'), method: :delete))
+    if current_user.find_friendships_pending(user_id).first &&
+       current_user.find_friendships_pending(user_id).first.receiver == current_user
+      content_tag(:li, link_to('Accept friendship',
+                               friendship_path(id: user_id), method: :put)) +
+        content_tag(:li, link_to('Reject friendship',
+                                 friendship_path(id: user_id, status: 'pending'), method: :delete))
     else
       content_tag(:li, 'request pending')
     end
