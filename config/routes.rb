3efd87fadeae129f2e879619d99f9a7cd9661a7a
abdellani <abdellani@gmail.com>
2019-09-02 17:44:53 +0100

    Add omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
 
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   resources :comments, only: %i[create destroy update edit]
   resources :likes, only: %i[create destroy]
   resources :friendships, only: %i[create update destroy]
+
 end
